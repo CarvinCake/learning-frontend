d453f8164218dae52832544bba78cfa5f4bf6d85
Arseny <orsundel@gmail.com>
2023-01-11 21:53:51 +0200

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,68 @@
 const navigationButtonClass = "navigation__button";
-const navigationButtons = document.getElementsByClassName(navigationButtonClass);
+const navigationButtons = document.getElementsByClassName(
+  navigationButtonClass
+);
 
-type NavigationSection =
-	{
-		title: string;
-		name: string;
-		innerHTML: string;
-	}
-
-
-for (let i = 0; i < navigationButtons.length; i++){
-	const button = navigationButtons[i];
-	button.addEventListener("click", function (this: typeof button) {
-		const activeButtonClass = "navigation__button--current";
-		if (this.classList.contains(activeButtonClass))
-			return;
-		const currentButton = document.getElementsByClassName(activeButtonClass);
-		currentButton[0].classList.remove(activeButtonClass);
-		this.classList.add(activeButtonClass);
-		let newSection: string;
-		switch (this.textContent) {
-			case "Якась гра":
-				newSection = gameSection;
-				break;
-			case "Державний":
-				break;
-			default:
-				break;
-		}
-		document.getElementsByClassName("content")[0].innerHTML = newSection;
-	})
+class NavigationSection {
+  name: string;
+  buttonTitle: string;
+  htmlElement: HTMLElement;
+  constructor(name: string, buttonTitle: string) {
+    const newElement = document.createElement("div");
+    this.name = name;
+    this.buttonTitle = buttonTitle;
+    this.htmlElement = newElement;
+  }
+  createGameSection() {
+    this.htmlElement.id = "app";
+    const script = this.htmlElement.appendChild(
+      document.createElement("script") as Node
+    ) as HTMLScriptElement;
+    script.src = "src/Game/game.ts";
+    script.type = "module";
+    return this;
+  }
+  createParagraph() {
+    this.htmlElement = document.createElement("p");
+    this.htmlElement.innerText = "Hello";
+    return this;
+  }
 }
 
-const gameSection = '<div id="app"></div><script type="module" src="src/Game/game.ts"></script>';
+const gameSection = new NavigationSection(
+  "Game",
+  "Якась гра"
+).createGameSection();
+const anthemSection = new NavigationSection(
+  "Anthem",
+  "Державний Гімн України"
+).createParagraph();
 
-export {}
+for (let i = 0; i < navigationButtons.length; i++) {
+  const NavigationSection = [gameSection, anthemSection];
+  const button = navigationButtons[i];
+  button.textContent = NavigationSection[i].buttonTitle;
+  button.addEventListener("click", function (this: typeof button) {
+    const activeButtonClass = "navigation__button--current";
+    if (this.classList.contains(activeButtonClass)) return;
+    const currentButton = document.getElementsByClassName(activeButtonClass);
+    currentButton[0].classList.remove(activeButtonClass);
+    this.classList.add(activeButtonClass);
+    let newSection: HTMLElement;
+    switch (this.textContent) {
+      case gameSection.buttonTitle:
+        newSection = gameSection.htmlElement;
+        break;
+      case anthemSection.buttonTitle:
+        newSection = anthemSection.htmlElement;
+        break;
+      default:
+        break;
+    }
+    const content = document.getElementsByClassName("content")[0];
+    content.innerHTML = "";
+    content.appendChild(newSection);
+  });
+}
+
+export {};
