7c1ea6245917d24e88bda866529bf4a6e061ad9b
Arseny <orsundel@gmail.com>
2023-01-12 05:32:59 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   plugins: [],
   server: { host: "0.0.0.0", port: 8000 },
   clearScreen: false,
-  base: "/vite-deploy-demo/", // fix for asset links
+  //   base: "/vite-deploy-demo/", // fix for asset links
 });
